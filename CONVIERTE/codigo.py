a35b0436a8e4d56b04908b8298d4a0b2835fcb39
JesusNolasco35 <24308060610037@cetis61.edu.mx>
2025-04-10 09:53:39 -0600

    convertidor final

--- CONVIERTE/codigo.py
+++ CONVIERTE/codigo.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-root = tk.Tk()
-root.title("Jesus Eduardo Nolasco Flores")
-root.geometry("530x300")
-root.resizable(False, False)
-root.configure(bg="blue")
+ventana = tk.Tk()
+ventana.title("Jesus Eduardo Nolasco Flores")
+ventana.geometry("530x350")
+ventana.configure(bg="gray")
 
-tk.Label(root, text="Ingresa un Número Entero", bg="#1C1C1E", fg="#C0C0C0", font=("Arial", 20, "bold"), justify="center").pack(pady=(20, 10))
-resultado_label = tk.Label(root, text="", bg="#1C1C1E", fg="#D4AF37", font=("Arial", 14)); resultado_label.pack(pady=(10, 20))
-
+Label=tk.Label(ventana, text="Ingresa un Número Entero", bg="#1C1C1E", fg="#C0C0C0", font=("Arial", 20, "bold"))
+Label.pack()
+resultado_label = tk.Label(ventana, text="", bg="#1C1C1E", fg="#D4AF37", font=("Arial", 14))
+resultado_label.pack()
 def toBin():
     try:
         num = int(entrada.get())
@@ -17,8 +17,7 @@ def toBin():
             num //= 2
         resultado_label.config(text=f"Binario: {resultado}")
     except ValueError:
-        resultado_label.config(text="Error: Por favor, introduce un número entero.", fg="red")
-
+        resultado_label.config(text="Error: Por favor, introduce un número entero.")
 def toOctal():
     try:
         num = int(entrada.get())
@@ -28,8 +27,7 @@ def toOctal():
             num //= 8
         resultado_label.config(text=f"Octal: {resultado}")
     except ValueError:
-        resultado_label.config(text="Error: Por favor, introduce un número entero.", fg="red")
-
+        resultado_label.config(text="Error: Por favor, introduce un número entero.")
 def toHexa():
     try:
         num = int(entrada.get())
@@ -40,18 +38,18 @@ def toHexa():
             num //= 16
         resultado_label.config(text=f"Hexadecimal: {resultado}")
     except ValueError:
-        resultado_label.config(text="Error: Por favor, introduce un número entero.", fg="red")
+        resultado_label.config(text="Error: Por favor, introduce un número entero.")
 
 
-
-entrada = tk.Entry(root, justify="center", bg="#2C2C2E", fg="#F2F2F2", insertbackground="#F2F2F2", width=20, font=("Arial", 14), relief="solid", bd=2)
+entrada=tk.Entry(ventana, justify="center", bg="#2C2C2E", fg="#F2F2F2", insertbackground="#F2F2F2", width=20, font=("Arial", 14), relief="solid", bd=2)
 entrada.pack(pady=(5, 20))
 
 
-boton = tk.Button(root, text="Convertir A Binario", bg="white", fg="black", width=15, height=2, command=toBin, relief="flat").pack(side="left", padx=(75, 5), pady=10)
-boton2 = tk.Button(root, text="Convertir A Octal", bg="white", fg="black", width=15, height=2, command=toOctal, relief="flat").pack(side="left", padx=5, pady=10)
-boton3 = tk.Button(root, text="Convertir A Hexadecimal", bg="white", fg="black", width=20, height=2, command=toHexa, relief="flat").pack(side="left", padx=5, pady=10)
+boton = tk.Button(ventana, text="Convertir A Binario", bg="white", fg="black", width=15, height=2, command=toBin)
+boton.pack()
+boton2 = tk.Button(ventana, text="Convertir A Octal", bg="white", fg="black", width=15, height=2, command=toOctal)
+boton2.pack()
+boton3 = tk.Button(ventana, text="Convertir A Hexadecimal", bg="white", fg="black", width=20, height=2, command=toHexa)
+boton3.pack()
 
-
-
-root.mainloop()
+ventana.mainloop()
